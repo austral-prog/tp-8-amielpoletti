8c2a5174f3976ea9e49c6454e5b8032fc1200456
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-05-29 20:27:59 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -8,17 +8,18 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    return ""
+    _, coordinate = record
+    return coordinate
 
 
-def convert_coordinate(coordinate):
+def convert_coordinate(coordinate: str):
     """Split the given coordinate into tuple containing its individual components.
 
     :param coordinate: str - a string map coordinate
     :return: tuple - the string coordinate split into its individual components.
     """
 
-    return ()
+    return (coordinate[0], coordinate[1])
 
 
 def create_record(azara_record, rui_record):
@@ -28,5 +29,11 @@ def create_record(azara_record, rui_record):
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
+    
+    az_tesoro, az_coordenada = azara_record
+    rui_ubicación, rui_coordenada, rui_cuadrante = rui_record
 
-    return ()
+    if az_coordenada[0] != rui_coordenada[0] or az_coordenada[1] != rui_coordenada[1] :
+        return "not a match"
+
+    return (az_tesoro, az_coordenada, rui_ubicación, rui_coordenada, rui_cuadrante)
